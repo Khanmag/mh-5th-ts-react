bcd9e854ce40b6ea703387d2735de5b2e99f9f9b
Shamil <khanmag329@gmail.com>
2024-05-21 15:28:35 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import L01 from "./L01";
+
+function App() {
+  return (
+    <>
+    <L01 />
+      fadsgsdfsdafsd
+    </>
+  );
+}
+
+export default App;
---
+++ src/L01/index.tsx
@@ -0,0 +1,76 @@
+const L01 = () => {
+  //В формате React проекта мы будем писать TypeScript код в подобных функциях
+  // let a: number = 123;
+  // a = 43
+  // a = "adfasdf"
+  // let b: string = "dfasdfsd"
+  // b = "fasdfsd"
+  // b = undefined
+  // let bool: boolean | string = true;
+  // bool = "Afasdfasd"
+  // let bigNum: bigint = 1234123452314331223452345234n;
+
+  // type UserType = {
+  //   login: string;
+  //   pin: number;
+  // };
+  // const user1: UserType = {
+  //   login: "qwerty",
+  //   pin: 123423,
+  // };
+  // type PersonType = {
+  //   name: string;
+  //   age?: number;
+  //   adress?: {
+  //     city: string;
+  //   };
+  //   getName?: (greeting: string) => void;
+  // };
+  // const person1: PersonType = {
+  //   name: "Anna",
+  //   age: 30,
+  //   adress: {
+  //     city: "adsfsad",
+  //   },
+  //   getName(greeting) {
+  //     console.log(`${greeting}, ${this.name}`);
+  //   },
+  // };
+  // const userPerson: UserType & PersonType = {
+  //   login: "qwerty",
+  //   pin: 123423,
+  //   name: "Ben",
+  // };
+
+  // const hideData: unknown = "34123";
+  // const strData: string = "34123";
+  // strData[4];
+  // (hideData as string)[4];
+  // (hideData as PersonType).name;
+  // const someData: any = "gewsrywer";
+  // someData.map();
+  // strData.map()
+
+  // const loadStatus: "pending" | "fullfiled" | "rejected" = "pending"
+  // enum LoadStatus {
+  //   pending,
+  //   fullfiled,
+  //   rejected,
+  // }
+  // const downloadStatus = LoadStatus.pending;
+
+  // const tuple1: [any, number, string] = [113, 123, "123"];
+
+  // const someFunc = (n: number, k: string): string => {
+  //   return n + k;
+  // };
+  // const someStr: string = someFunc(3, "5")
+  // const someNum: number = someFunc(3)
+  const test: string = "qwerty"
+  console.log(test);
+  
+  //
+  return <div>L01</div>;
+};
+
+export default L01;
