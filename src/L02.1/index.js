487f2a6b72af5e9d5406b55469092f8e5e803f0a
Shamil <khanmag329@gmail.com>
2024-05-26 20:30:26 +0300

    L02.1

--- src/L02.1/index.js
+++ src/L02.1/index.js
@@ -24,10 +24,37 @@ const addUserToPage = (name, rootList) => {
 
 const addUsersToPage = (userList) => {
   const newUserList = document.createElement("ul");
-  userList.forEach( user => {
-    addUserToPage(user, newUserList)
-  })
-  rootNode.append(newUserList) // добавляем ul в div c id root
-}
+  userList.forEach((user) => {
+    addUserToPage(user, newUserList);
+  });
+  rootNode.append(newUserList); // добавляем ul в div c id root
+};
 
-addUsersToPage(["Madina", "Olesya", "Sasha"])
+// addUsersToPage(["Madina", "Olesya", "Sasha"])
+
+// Задание - создать функцию, которая добавить нумерованный список дел на день
+// addTodoDaity(["Встать", "Позавтракать", "Поработать", "Поспать",])
+// 1. Встать
+// 2. Позавтракать
+// 3. Поработать
+// 4. Поспать
+
+// const image = document.createElement("img");
+// image.src =
+//   "https://mirpozitiva.ru/wp-content/uploads/2019/11/1476889932_zakat-derevo.jpg";
+// console.dir(image);
+// rootNode.append(image);
+
+// https://mirpozitiva.ru/wp-content/uploads/2019/11/1476889932_zakat-derevo.jpg
+
+const arr = [
+  "https://mirpozitiva.ru/wp-content/uploads/2019/11/1476889932_zakat-derevo.jpg",
+  "https://mykaleidoscope.ru/x/uploads/posts/2022-10/1666389897_62-mykaleidoscope-ru-p-klassnaya-priroda-oboi-67.jpg",
+  "https://www.w-dog.ru/wallpapers/11/2/528819469500993/kanada-joxo-ozero-les-gory-skaly-sneg.jpg",
+];
+
+arr.forEach((item) => {
+  const image = document.createElement("img");
+  image.src = item;
+  rootNode.append(image);
+});
