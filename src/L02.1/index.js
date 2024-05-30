9b242a56ee9387e80ac0c155b610038b3e8b2d5a
Shamil <khanmag329@gmail.com>
2024-05-30 20:06:44 +0300

    L04 mw

--- src/L02.1/index.js
+++ src/L02.1/index.js
@@ -1,4 +1,4 @@
-const rootNode = document.getElementById("root");
+// const rootNode = document.getElementById("root"); // нахдим div с id = root
 
 // rootNode.textContent = "ROOT";
 // rootNode.innerText = "ROOT"
@@ -8,45 +8,45 @@ const rootNode = document.getElementById("root");
 // console.log(rootNode.innerText);
 // console.log(rootNode.innerHTML);
 
-const addUserToPage = (name, rootList) => {
-  // rootNode.textContent = rootNode.textContent + " " + name;
-  // // rootNode.textContent += " " + name;
-  const newUser = document.createElement("li");
-  newUser.textContent = name;
-  // rootNode.append(newUser);
-  rootList.append(newUser); // добавляем li в ul (rootList)
-};
+// const addUserToPage = (name, rootList) => {
+//   // rootNode.textContent = rootNode.textContent + " " + name;
+//   // // rootNode.textContent += " " + name;
+//   const newUser = document.createElement("li");
+//   newUser.textContent = name;
+//   // rootNode.append(newUser);
+//   rootList.append(newUser); // добавляем li в ul (rootList)
+// };
 
-// addUserToPage("Anna");
-// addUserToPage("Bill");
-// addUserToPage("Clint");
-// addUserToPage("Den");
+// // addUserToPage("Anna");
+// // addUserToPage("Bill");
+// // addUserToPage("Clint");
+// // addUserToPage("Den");
 
-const addUsersToPage = (userList) => {
-  const newUserList = document.createElement("ul");
-  userList.forEach((user) => {
-    addUserToPage(user, newUserList);
-  });
-  rootNode.append(newUserList); // добавляем ul в div c id root
-};
+// const addUsersToPage = (userList) => {
+//   const newUserList = document.createElement("ul");
+//   userList.forEach((user) => {
+//     addUserToPage(user, newUserList);
+//   });
+//   rootNode.append(newUserList); // добавляем ul в div c id root
+// };
 
-// addUsersToPage(["Madina", "Olesya", "Sasha"])
+// // addUsersToPage(["Madina", "Olesya", "Sasha"])
 
-const getUsersList = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await response.json();
-  // console.log(data);
-  // console.log([1, 2, 3, 4].map( (item, index, arr) => {
-  //   return item * 2
-  // }));
-  const correctArr = data.map((item) => {
-    return item.name;
-  });
-  console.log(correctArr);
-  addUsersToPage(correctArr)
-};
+// const getUsersList = async () => {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const data = await response.json();
+//   // console.log(data);
+//   // console.log([1, 2, 3, 4].map( (item, index, arr) => {
+//   //   return item * 2
+//   // }));
+//   const correctArr = data.map((item) => {
+//     return item.name;
+//   });
+//   console.log(correctArr);
+//   addUsersToPage(correctArr);
+// };
 
-getUsersList();
+// getUsersList();
 
 // Задание - создать функцию, которая добавить нумерованный список дел на день
 // addTodoDaity(["Встать", "Позавтракать", "Поработать", "Поспать",])
@@ -69,10 +69,48 @@ const arr = [
   "https://www.w-dog.ru/wallpapers/11/2/528819469500993/kanada-joxo-ozero-les-gory-skaly-sneg.jpg",
 ];
 
-arr.forEach((item) => {
-  const image = document.createElement("img");
-  image.src = item;
-  image.className = "picture";
-  image.style.width = "100px";
-  rootNode.append(image);
-});
+// arr.forEach((item) => {
+//   const image = document.createElement("img");
+//   image.src = item;
+//   image.className = "picture";
+//   image.style.width = "100px";
+//   rootNode.append(image);
+// });
+
+const addPhoto = (content, container) => {
+  // < img />
+  const image = document.createElement("img"); // image ==> < img />
+  const div = document.createElement("div"); // div ==> < div />
+
+  // ** DOM Document Object Model
+  // console.dir(document);
+
+  // < img src="..." />
+  image.src = content.src;
+  image.alt = content.alt;
+  image.title = content.title;
+  // image.style.width = "400px"
+  div.className = "imageBox";
+
+  // <div> < img src="..." /> </div>
+  div.append(image);
+  // <> <div> < img src="..." /> </div> </>
+  container.append(div);
+};
+
+const addPhotosBox = (photosList) => {
+  const rootNode = document.getElementById("root"); // нахдим div с id = root
+  const imageContainer = document.createElement("section"); // <section />
+  rootNode.append(imageContainer); // <div id="root"><section /></div>
+  // Пробегаемся по массиву и для каждого элемента вызываем функцию addPhoto - те добавляем фото
+  photosList.forEach((item) => {
+    addPhoto(
+      {
+        src: item,
+      },
+      imageContainer
+    );
+  });
+};
+
+addPhotosBox(arr)
